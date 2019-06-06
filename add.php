06858a5af44e6dbb3d61d2e9ba4cf3d0fe2e0fec
samikshawartak <40663091+samikshawartak@users.noreply.github.com>
2019-06-07 00:10:04 +0530

    Add files via upload

--- add.php
+++ add.php
@@ -1,4 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+	<?php
 $mach = "localhost";
 $name = "root";
 $pass = "";
@@ -9,8 +17,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO users (name, email, phone,query)
-VALUES ('$_POST[nam]','$_POST[email]','$_POST[phone]','$_POST[query]')";
+$sql = "INSERT INTO users (name, email, phone, query)
+VALUES ('$_POST[nam]', '$_POST[email]', '$_POST[phone]','$_POST[query]')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Query submitted";
@@ -20,3 +28,6 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
+<a href="index.php">Return to home page</a>
+</body>
+</html>
